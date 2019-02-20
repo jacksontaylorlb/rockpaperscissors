3c68e39ced423aa7df2a8e5935d7d20a38777085
jacksontaylorlb <46732062+jacksontaylorlb@users.noreply.github.com>
2019-02-19 16:15:02 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = (Math.floor(Math.random()*3));
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+
+function win(userChoice, computerChoice){
+   userScore++;
+   userScore_span.innerHTML = userScore;
+   compScore_span.innerHTML = computerScore;
+   result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + " you win";
+}
+
+
+function lose(userChoice, compouterChoice){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(compouterChoice) + " beats " + convertToWord(user) + " you lose";
+
+}
+
+function draw(userChoice, computerChoice){
+    result_p.innerHTML = "Its a Draw"
+}
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+
+    switch (userChoice + computerChoice) {
+        case"rs":
+        case"pr":
+        case"sp":
+            win(userChoice, computerChoice);
+            break;
+        case"rp":
+        case"ps":
+        case"sr":
+            lose(userChoice, computerChoice);
+            break;
+        case"rr":
+        case"pp":
+        case"ss":
+            draw(userChoice, computerChoice);
+            break;
+
+    }
+}
+
+
+
+function main(){
+
+
+    rock_div.addEventListener('click', function(){
+        game("r")
+    })
+
+    paper_div.addEventListener('click', function(){
+        game("p")
+    })
+
+    scissors_div.addEventListener('click', function(){
+        game("s")
+    })
+
+}
+main();
+
